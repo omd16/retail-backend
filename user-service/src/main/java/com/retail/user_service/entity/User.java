323d2bdc88fb8a9d37c0dee4d222f47abf8e5eb2
Omkar <omkardhaybar1998@gmail.com>
2025-09-03 22:18:27 +0530

    user service: imports refactored

--- user-service/src/main/java/com/retail/user_service/entity/User.java
+++ user-service/src/main/java/com/retail/user_service/entity/User.java
@@ -1,15 +1,11 @@
 package com.retail.user_service.entity;
 
 import jakarta.persistence.*;
+import java.util.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.time.LocalDateTime;
-import java.util.*;
 
 @Entity
 @Getter
@@ -17,33 +13,34 @@ import java.util.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User extends Auditable<String>{
+public class User extends Auditable<String> {
 
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    private UUID id;
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  private UUID id;
 
-    private String name;
+  private String name;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @Column(name = "password_hash", nullable = false)
-    private String passwordHash;
+  @Column(name = "password_hash", nullable = false)
+  private String passwordHash;
 
-    @Column(unique = true)
-    private String phoneNumber;
+  @Column(unique = true)
+  private String phoneNumber;
 
-    private boolean isEmailVerified = false;
+  private boolean isEmailVerified = false;
 
-    private boolean isActive = true;
+  private boolean isActive = true;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<UserAddress> addresses = new ArrayList<>();
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<UserAddress> addresses = new ArrayList<>();
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name = "user_roles",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles = new HashSet<>();
 }
